961a204621e078d7aaa0e31a930bff7a1246470f
steve chun (haechul chun) <guruchun@gmail.com>
2022-11-22 11:33:00 +0900

    update day4 start code

--- SimCogen/Day4.cs
+++ SimCogen/Day4.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Data;
+using System.Diagnostics;
+using FcpUtils;
 using System.Xml.Linq;
+using Timer = System.Windows.Forms.Timer;
+using static System.Windows.Forms.AxHost;
 
 namespace SimCogen
 {
     public partial class Day4 : Form
     {
-        private DataTable _DataList = new DataTable();
+        private DataTable tblCogen = new DataTable("DtCogen");
+        private DataTable[] tblFCell = new DataTable[6];
+        //private Timer updateTimer = new Timer();
 
         public Day4()
         {
@@ -27,92 +24,253 @@ namespace SimCogen
             // set table columns
             string[] colName = { "Name", "Value", "Description" };
             string[] colType = { "System.String", "System.Object", "System.String" };
-            // for _DataList
+            // for tblCogen
             for (int i = 0; i < colName.Length; i++)
             {
                 DataColumn col = new DataColumn(colName[i], dataType: Type.GetType(colType[i]) ?? typeof(String));
-                _DataList.Columns.Add(col);
+                tblCogen.Columns.Add(col);
             }
             // for searching, filtering
-            _DataList.PrimaryKey = new DataColumn[] { (_DataList.Columns["Name"] ?? _DataList.Columns[0]) };
-            DgvCogen.DataSource = _DataList;
+            tblCogen.PrimaryKey = new DataColumn[] { (tblCogen.Columns["Name"] ?? tblCogen.Columns[0]) };
+            DgvCogen.DataSource = tblCogen;
+
+            // for tblFCell
+            for (int fc = 0; fc < 6; fc++)
+            {
+                this.tblFCell[fc] = new DataTable("DtFCell-" + fc);
+                for (int i = 0; i < colName.Length; i++)
+                {
+                    DataColumn col = new DataColumn(colName[i], dataType: Type.GetType(colType[i]) ?? typeof(String));
+                    tblFCell[fc].Columns.Add(col);
+                }
+                tblFCell[fc].PrimaryKey = new DataColumn[] { tblFCell[fc].Columns["Name"] ?? tblFCell[fc].Columns[0] };
+            }
+            DgvFC.DataSource = tblFCell[0];
+        }
+
+        private void LoadDataTable()
+        {
+            // 임시
+            string fileFullName = "config/init-data-cogen.txt";
+            // open property file
+            Property prop = new(fileFullName);
+
+            // update DataGridView directly
+            //foreach (DataGridViewRow row in DgvCogen.Rows)
+            //{
+            //    // get tag value
+            //    string tag = (string)row.Cells[3].Value;
+            //    if (String.IsNullOrEmpty(tag))
+            //    {
+            //        row.Cells[1].Value = "0";
+            //    }
+            //    else
+            //    {
+            //        row.Cells[1].Value = prop.Get(tag, "0");
+            //    }
+            //}
+
+            // update DataTable
         }
 
         private void Day4_Load(object sender, EventArgs e)
         {
+            // set up DataTable
             SetupDataTable();
-            _DataList.Rows.Add(new object[] { "test", "value", "description" });
-            _DataList.Rows.Add(new object[] { "test2", "value2", "description2" });
 
-            foreach (var c in this.Controls)
+            // set up PictureBox
+            List<PictureBox> picList = new();
+            picList.AddRange(this.Controls.OfType<PictureBox>());
+            foreach (PictureBox pb in picList)
             {
-                if (c is PictureBox)
+                if (pb.Name.StartsWith("Pic"))
                 {
-                    PictureBox pb = (PictureBox)c;
-                    if (pb.Name.StartsWith("Pic"))
-                    {
-                        _DataList.Rows.Add(new object[] { pb.Name, "0", "description" });
-                        pb.Click += new System.EventHandler(this.Pic_Click);
-                    }
+                    Debug.WriteLine($"PicBox Name={pb.Name}");
+
+                    // set transparent of background
+                    pb.Parent = CogenBG;    // Form의 Child Controls에 영향을 주므로 직접 접근하지 않고 복사해 놓고 사용한다.
+                    pb.Location = new Point(pb.Location.X - CogenBG.Location.X, pb.Location.Y - CogenBG.Location.Y);
+
+                    // add event handler
+                    pb.Click += new EventHandler(PictureBox_Click);
+
+                    // add name on tblCogen
+                    string name = pb.Name[3..];
+                    tblCogen.Rows.Add(new object[] { name, 0 });
+                }
+                else
+                {
+                    Debug.WriteLine($"PicOther Name={pb.Name}");
                 }
             }
 
-            FormRefresh();
-        }
-
-        private void FormRefresh()
-        {
-            //TxtFwVer.Text = (string)_DataList[TxtFwVer.Name];
-            //TxtPit02.Text = (string)_DataList[TxtPit02.Name];
-
-
-            foreach (var c in this.Controls)
+            // set up TextBox
+            foreach (TextBox tb in this.Controls.OfType<TextBox>())
             {
-                if (c is PictureBox)
+                if (tb.Name.StartsWith("Txt"))
                 {
-                    PictureBox pb = (PictureBox)c;
-                    string name = pb.Name;
-                    if (name.StartsWith("Pic"))
-                    {
-                        DataRow? row = _DataList.Rows.Find(name);
-                        string value = (string)row[1];
-                        if (value == "1")
-                            pb.BackgroundImage = Properties.Resources.dot_green;
-                        else
-                            pb.BackgroundImage = Properties.Resources.dot_red;
-                    }
+                    Debug.WriteLine($"TextBox Name={tb.Name}");
+
+                    // add event handler
+                    tb.TextChanged += new EventHandler(TextBox_TextChanged);
+
+                    // add name on dictionary
+                    string name = tb.Name[3..];
+                    tblCogen.Rows.Add(new object[] { name, "" });
+                }
+                else
+                {
+                    Debug.WriteLine($"TxtOther Name={tb.Name}");
                 }
             }
+
+            // ComboBox 초기화
+            // design time에 입력한 item을 제거한다.
+            this.FcID.Items.Clear();
+            // configuration에 따라 FC의 개수만큼 설정한다.
+            List<string> fcList = new() { "01", "02", "03", "04", "05", "06" };
+            this.FcID.Items.AddRange(fcList.ToArray());
+            this.FcID.SelectedIndex = 0;
+
+            // Loading된 데이터로 초기화한다.
+            LoadDataTable();
+
+            // 화면을 업데이트하기 위해 Timer를 활성화한다.
+            //updateTimer.Interval = 500;
+            //updateTimer.Tick += UpdateTimer_Tick;
+            //updateTimer.Enabled = true;
+            //updateTimer.Start();
         }
 
-        private void Txt_TextChanged(object sender, EventArgs e)
+        private void UpdateTimer_Tick(object? sender, EventArgs e)
         {
-            //_DataList["TxtFwVer"] = TxtFwVer.Text;
-            FormRefresh();
+            Form_Refresh();
         }
 
-        private void Pic_Click(object sender, EventArgs e)
+        private void UpdatePicBoxes(object picParent)
         {
-            PictureBox? pb = sender as PictureBox;
-            if (pb == null)
+            if (picParent == null)
                 return;
 
-            string name = pb.Name;
-            var row = _DataList.Rows.Find(name);
-            string value = (string)row[1];
-            if (value == "1")
-                value = "0";
-            else
-                value = "1";
+            PictureBox? pa = picParent as PictureBox;
+            if (pa == null)
+                return;
 
-            row[1] = value;
+            foreach (var c in pa.Controls)
+            {
+                if (c is PictureBox pb)
+                {
+                    if (pb.Name.StartsWith("Pic"))
+                    {
+                        string name = pb.Name[3..];
+                        DataRow? row = tblCogen.Rows.Find(name);
+                        if (row is not null)
+                        {
+                            Int32 state = 0;
+                            if (row[1] is string)
+                            {
+                                Int32.TryParse((string)row[1], out state);
+                            }
+                            else if (row[1] is Int32)
+                            {
+                                state = (Int32)row[1];
+                            }
+                            else
+                            {
+                                Debug.Print("invalid value {0}", row[1].ToString());
+                                continue;
+                            }
 
-            FormRefresh();
+                            // set image by state
+                            if (state > 0)
+                            {
+                                pb.BackgroundImage = Properties.Resources.dot_green;
+                            }
+                            else
+                            {
+                                pb.BackgroundImage = Properties.Resources.dot_red;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        private void DgvCogen_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        private void Form_Refresh()
         {
-            FormRefresh();
+            foreach (var c in this.Controls)
+            {
+                if (c is TextBox tb)
+                {
+                    if (tb.Name.StartsWith("Txt"))
+                    {
+                        string name = tb.Name[3..];
+                        DataRow? row = tblCogen.Rows.Find(name);
+                        if (row is not null)
+                            tb.Text = (string)row[1];   // value
+                    }
+                }
+                else if (c is PictureBox pb)
+                {
+                    UpdatePicBoxes(pb);
+                }
+                else
+                {
+                    // ignore the other controls
+                }
+            } // end of foreach
+        }
+
+        private void TextBox_TextChanged(object? sender, EventArgs e)
+        {
+            if (sender is not TextBox txtBox)
+                return;
+
+            string name = txtBox.Name[3..];
+            Debug.WriteLine($"TextBox {name} = {txtBox.Text}");
+
+            Double value;
+            bool isOk = Double.TryParse((string)txtBox.Text, out value);
+            if (!isOk)
+            {
+                Debug.Print("invalid value {0}", txtBox.Text);
+                return;
+            }
+
+            // update on DataTable
+            DataRow? row = tblCogen.Rows.Find(name);
+            if (row is not null)
+            {
+                row[1] = txtBox.Text;
+            }
+        }
+
+        private void PictureBox_Click(object? sender, EventArgs e)
+        {
+            if (sender is not PictureBox picBox)
+                return;
+
+            string name = picBox.Name[3..];
+
+            // get current state
+            DataRow? row = tblCogen.Rows.Find(name);
+            if (row is not null)
+            {
+                Int32 state = 0;
+                if (row[1] is string)
+                {
+                    Int32.TryParse((string)row[1], out state);
+                }
+                else if (row[1] is Int32)
+                {
+                    state = (Int32)row[1];
+                }
+                else
+                {
+                    Debug.Print("invalid value {0}", row[1].ToString());
+                }
+                row[1] = state > 0 ? 0 : 1;
+            }
         }
     }
 }
